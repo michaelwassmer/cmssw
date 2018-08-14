047f4a5606039118218f16fa8b23bc1673b38af0
Michael Wassmer <michael.wassmer@cern.ch>
2018-08-14 12:34:35 +0200

    changed SmearedJetProducer in 94X to our needs

--- PhysicsTools/PatUtils/interface/SmearedJetProducerT.h
+++ PhysicsTools/PatUtils/interface/SmearedJetProducerT.h
@@ -32,6 +32,7 @@
 #include <TFile.h>
 #include <TH1.h>
 #include <TH1F.h>
+#include <TRandom3.h>
 
 #include <memory>
 #include <random>
@@ -126,6 +127,7 @@ class SmearedJetProducerT : public edm::stream::EDProducer<> {
 
                 std::uint32_t seed = cfg.getParameter<std::uint32_t>("seed");
                 m_random_generator = std::mt19937(seed);
+                m_random_generator_alt = TRandom3();
 
                 bool skipGenMatching = cfg.getParameter<bool>("skipGenMatching");
                 if (! skipGenMatching)
@@ -194,7 +196,8 @@ class SmearedJetProducerT : public edm::stream::EDProducer<> {
             JME::JetResolution resolution;
             JME::JetResolutionScaleFactor resolution_sf;
 
-            const JetCollection& jets = *jets_collection;
+            // use non-const jetcollection compared to unchanged SmearedJetProducer to be able to add userfloats
+            JetCollection jets = *jets_collection;
 
             if (m_enabled) {
                 if (m_use_txt_files) {
@@ -219,8 +222,8 @@ class SmearedJetProducerT : public edm::stream::EDProducer<> {
                 m_genJetMatcher->getTokens(event);
 
             auto smearedJets = std::make_unique<JetCollection>();
-
-            for (const auto& jet: jets) {
+            // therefore use non-const iterator over jets in jetcollection
+            for (auto& jet: jets) {
 
                 if ((! m_enabled) || (jet.pt() == 0)) {
                     // Module disabled or invalid p4. Simply copy the input jet.
@@ -228,61 +231,24 @@ class SmearedJetProducerT : public edm::stream::EDProducer<> {
 
                     continue;
                 }
+                double smearFactor = GetCorrection(jet,m_systematic_variation,resolution,resolution_sf,*rho);
+                double smearFactor_nom = GetCorrection(jet,Variation::NOMINAL,resolution,resolution_sf,*rho);
 
-                double jet_resolution = resolution.getResolution({{JME::Binning::JetPt, jet.pt()}, {JME::Binning::JetEta, jet.eta()}, {JME::Binning::Rho, *rho}});
-                double jer_sf = resolution_sf.getScaleFactor({{JME::Binning::JetEta, jet.eta()}}, m_systematic_variation);
-
-                if (m_debug) {
-                    std::cout << "jet:  pt: " << jet.pt() << "  eta: " << jet.eta() << "  phi: " << jet.phi() << "  e: " << jet.energy() << std::endl;
-                    std::cout << "resolution: " << jet_resolution << std::endl;
-                    std::cout << "resolution scale factor: " << jer_sf << std::endl;
+                // add JER SFs as user floats to jets to be able to use later
+                switch(m_systematic_variation) {
+                    
+                    case Variation::NOMINAL:
+                        jet.addUserFloat("HelperJER",smearFactor);
+                        break;
+                    case Variation::UP:
+                        jet.addUserFloat("HelperJER",smearFactor_nom);
+                        jet.addUserFloat("HelperJERup",smearFactor/smearFactor_nom);
+                        break;
+                    case Variation::DOWN:
+                        jet.addUserFloat("HelperJER",smearFactor_nom);
+                        jet.addUserFloat("HelperJERdown",smearFactor/smearFactor_nom);
+                        break;
                 }
-
-                const reco::GenJet* genJet = nullptr;
-                if (m_genJetMatcher)
-                    genJet = m_genJetMatcher->match(jet, jet.pt() * jet_resolution);
-
-                double smearFactor = 1.;
-
-                if (genJet) {
-                    /*
-                     * Case 1: we have a "good" gen jet matched to the reco jet
-                     */
-
-                    if (m_debug) {
-                        std::cout << "gen jet:  pt: " << genJet->pt() << "  eta: " << genJet->eta() << "  phi: " << genJet->phi() << "  e: " << genJet->energy() << std::endl;
-                    }
-
-                    double dPt = jet.pt() - genJet->pt();
-                    smearFactor = 1 + m_nomVar*(jer_sf - 1.) * dPt / jet.pt();
-
-                } else if (jer_sf > 1) {
-                    /*
-                     * Case 2: we don't have a gen jet. Smear jet pt using a random gaussian variation
-                     */
-
-                    double sigma = jet_resolution * std::sqrt(jer_sf * jer_sf - 1);
-                    if (m_debug) {
-                        std::cout << "gaussian width: " << sigma << std::endl;
-                    }
-
-                    std::normal_distribution<> d(0, sigma);
-                    smearFactor = 1. + m_nomVar*d(m_random_generator);
-                } else if (m_debug) {
-                    std::cout << "Impossible to smear this jet" << std::endl;
-                }
-
-                if (jet.energy() * smearFactor < MIN_JET_ENERGY) {
-                    // Negative or too small smearFactor. We would change direction of the jet
-                    // and this is not what we want.
-                    // Recompute the smearing factor in order to have jet.energy() == MIN_JET_ENERGY
-                    double newSmearFactor = MIN_JET_ENERGY / jet.energy();
-                    if (m_debug) {
-                        std::cout << "The smearing factor (" << smearFactor << ") is either negative or too small. Fixing it to " << newSmearFactor << " to avoid change of direction." << std::endl;
-                    }
-                    smearFactor = newSmearFactor;
-                }
-
                 T smearedJet = jet;
                 smearedJet.scaleEnergy(smearFactor);
 
@@ -297,6 +263,70 @@ class SmearedJetProducerT : public edm::stream::EDProducer<> {
             std::sort(smearedJets->begin(), smearedJets->end(), jetPtComparator);
 
             event.put(std::move(smearedJets));
+        }
+        
+        double GetCorrection(const auto& jet, const Variation& m_systematic_variation_, const JME::JetResolution& resolution, const JME::JetResolutionScaleFactor& resolution_sf, const double& rho) {
+            double smearFactor = 1.;
+            double jet_resolution = resolution.getResolution({{JME::Binning::JetPt, jet.pt()}, {JME::Binning::JetEta, jet.eta()}, {JME::Binning::Rho, rho}});
+            double jer_sf = resolution_sf.getScaleFactor({{JME::Binning::JetEta, jet.eta()}}, m_systematic_variation_);
+
+            if (m_debug) {
+                std::cout << "jet:  pt: " << jet.pt() << "  eta: " << jet.eta() << "  phi: " << jet.phi() << "  e: " << jet.energy() << std::endl;
+                std::cout << "resolution: " << jet_resolution << std::endl;
+                std::cout << "resolution scale factor: " << jer_sf << std::endl;
+            }
+
+            const reco::GenJet* genJet = nullptr;
+            if (m_genJetMatcher)
+                genJet = m_genJetMatcher->match(jet, jet.pt() * jet_resolution);
+
+            
+            if (genJet) {
+                /*
+                    * Case 1: we have a "good" gen jet matched to the reco jet
+                    */
+
+                if (m_debug) {
+                    std::cout << "gen jet:  pt: " << genJet->pt() << "  eta: " << genJet->eta() << "  phi: " << genJet->phi() << "  e: " << genJet->energy() << std::endl;
+                }
+
+                double dPt = jet.pt() - genJet->pt();
+                smearFactor = 1 + m_nomVar*(jer_sf - 1.) * dPt / jet.pt();
+
+            } else if (jer_sf > 1) {
+                /*
+                    * Case 2: we don't have a gen jet. Smear jet pt using a random gaussian variation
+                    */
+
+                double sigma = jet_resolution * std::sqrt(jer_sf * jer_sf - 1);
+                if (m_debug) {
+                    std::cout << "gaussian width: " << sigma << std::endl;
+                }
+                
+                std::normal_distribution<> d(0, sigma);
+                smearFactor = 1. + m_nomVar*d(m_random_generator);
+                
+                // if jet has deterministic seeds then use the Trandom3 geneerator and seed it with the corresponding deterministic seed
+                if(jet.hasUserInt("deterministicSeed")) {
+                    m_random_generator_alt.SetSeed((uint32_t)jet.userInt("deterministicSeed"));
+                    smearFactor = 1. + m_nomVar*m_random_generator_alt.Gaus(0, sigma);
+                }
+                
+            } else if (m_debug) {
+                std::cout << "Impossible to smear this jet" << std::endl;
+            }
+
+            if (jet.energy() * smearFactor < MIN_JET_ENERGY) {
+                // Negative or too small smearFactor. We would change direction of the jet
+                // and this is not what we want.
+                // Recompute the smearing factor in order to have jet.energy() == MIN_JET_ENERGY
+                double newSmearFactor = MIN_JET_ENERGY / jet.energy();
+                if (m_debug) {
+                    std::cout << "The smearing factor (" << smearFactor << ") is either negative or too small. Fixing it to " << newSmearFactor << " to avoid change of direction." << std::endl;
+                }
+                smearFactor = newSmearFactor;
+            }
+            return smearFactor;
         }
 
     private:
@@ -317,9 +347,11 @@ class SmearedJetProducerT : public edm::stream::EDProducer<> {
         std::unique_ptr<JME::JetResolutionScaleFactor> m_scale_factor_from_file;
 
         std::mt19937 m_random_generator;
+        TRandom3  m_random_generator_alt;
 
         GreaterByPt<T> jetPtComparator;
 
 	int m_nomVar;
 };
 #endif
+
